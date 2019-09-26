646d2e4cb0d528d8aea2b4dc2635a47c5f42d9a4
MrBretzel <Axelolo68@gmail.com>
2019-09-26 20:34:34 +0200

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -428,12 +428,32 @@ exports.profile_manifests = {
       var p = [];
 
       try {
+        var item = {};
 
-        console.log('Allo ?')
+        item['id'] = 'BuildTools-latest';
+        item['time'] = new Date().getTime();
+        item['releaseTime'] = new Date().getTime();
+        item['type'] = 'snapshot';
+        item['group'] = 'spigot';
+        item['webui_desc'] = 'Latest BuildTools.jar for building Spigot/Craftbukkit';
+        item['weight'] = 0;
+        item['filename'] = 'BuildTools.jar';
+        item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
+        item['version'] = 0;
+        item['release_version'] = '';
+        item['url'] = 'https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar';
 
-	       for (var index in body) {
-	           var profilesItem = new profile_template();
+        p.push(item);
+
+         for (var index in body) {
+             var profilesItem = new profile_template();
              var request = require('request');
+
+             console.log(body);
+
+             var url = 'https://mcmirror.io/api/file/spigot/{0}'.format(index);
+
+             console.log(url);
 
              request('https://mcmirror.io/api/file/spigot/{0}'.format(index), function(error, response, body){
                if(!error && response.statusCode == 200)
@@ -454,24 +474,7 @@ exports.profile_manifests = {
 
                  p.push(profilesItem);
                }});
-	       }
-
-        var item = {};
-
-        item['id'] = 'BuildTools-latest';
-        item['time'] = new Date().getTime();
-        item['releaseTime'] = new Date().getTime();
-        item['type'] = 'snapshot';
-        item['group'] = 'spigot';
-        item['webui_desc'] = 'Latest BuildTools.jar for building Spigot/Craftbukkit';
-        item['weight'] = 0;
-        item['filename'] = 'BuildTools.jar';
-        item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
-        item['version'] = 0;
-        item['release_version'] = '';
-        item['url'] = 'https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar';
-
-        p.push(item);
+         }
 
       } catch (e) {}
 
