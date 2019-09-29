496ce3770a169677f88f4136c8975c47d862c3fd
MrBretzel <Axelolo68@gmail.com>
2019-09-29 05:46:27 +0200

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -269,27 +269,40 @@ exports.profile_manifests = {
           var url = 'https://mcmirror.io/api/file/spigot/{0}'.format(body[index]);
           var filename = body[index];
 
-          var ref_obj = JSON.parse(url);
-          var item = new profile_template();
+          function get_json(url, callback) {
+            var request = require('request');
 
-          console.log('Test 1');
+            request.get({
+                url: url,
+                json: true,
+                headers: {'User-Agent': 'request'}
+              }, (err, res, data) => {
+                if (err) {
+                  console.log('Error:', err);
+                } else if (res.statusCode !== 200) {
+                  console.log('Status:', res.statusCode);
+                } else {
+                  var ref_obj = JSON.parse(data);
+                  var item = new profile_template();
 
-          item['id'] = index;
-          //item['time'] = new Date(ref_obj['date_epoch']).getTime();
-          //item['releaseTime'] = new Date(ref_obj['date_epoch']).getTime();
-          item['type'] = 'release';
-          item['group'] = 'spigot';
-          item['webui_desc'] = 'Spigot Build For Minecraft: {0}'.format(ref_obj['mc_version']);
-          item['weight'] = 5;
-          item['filename'] = filename;
-          item['downloaded'] = false;
-          item['version'] = ref_obj['mc_version'];
-          item['release_version'] = '';
-          item['url'] = ref_obj['direct_link'];
 
-          console.log('Test 2');
+                  item['id'] = index;
+                  item['time'] = new Date(ref_obj['date_epoch']).getTime();
+                  item['releaseTime'] = new Date(ref_obj['date_epoch']).getTime();
+                  item['type'] = 'release';
+                  item['group'] = 'spigot';
+                  item['webui_desc'] = 'Spigot Build For Minecraft: {0}'.format(ref_obj['mc_version']);
+                  item['weight'] = 5;
+                  item['filename'] = filename;
+                  item['downloaded'] = false;
+                  item['version'] = ref_obj['mc_version'];
+                  item['release_version'] = '';
+                  item['url'] = ref_obj['direct_link'];
 
-          p.push(item);
+                  p.push(item);
+                }
+            });
+          }
         }
       } catch (e) {
         console.log(e);
