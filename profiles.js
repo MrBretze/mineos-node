f5996dc06afbb75dcc64f74b5721d7ffdfa02256
MrBretzel <Axelolo68@gmail.com>
2019-09-29 06:33:34 +0200

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -269,26 +269,25 @@ exports.profile_manifests = {
           var url = 'https://mcmirror.io/api/file/spigot/{0}'.format(body[index]);
           var filename = body[index];
 
-          var ref_obj = JSON.parse(getjson(url, response)
-        {
-          var item = new profile_template();
+          getjson(url, function(error, response) {
+            var item = new profile_template();
+            var ref_obj = JSON.parse(response);
 
+            item['id'] = index;
+            item['time'] = new Date(ref_obj['date_epoch']).getTime();
+            item['releaseTime'] = new Date(ref_obj['date_epoch']).getTime();
+            item['type'] = 'release';
+            item['group'] = 'spigot';
+            item['webui_desc'] = 'Spigot Build For Minecraft: {0}'.format(ref_obj['mc_version']);
+            item['weight'] = 5;
+            item['filename'] = filename;
+            item['downloaded'] = false;
+            item['version'] = ref_obj['mc_version'];
+            item['release_version'] = '';
+            item['url'] =
 
-          item['id'] = index;
-          item['time'] = new Date(ref_obj['date_epoch']).getTime();
-          item['releaseTime'] = new Date(ref_obj['date_epoch']).getTime();
-          item['type'] = 'release';
-          item['group'] = 'spigot';
-          item['webui_desc'] = 'Spigot Build For Minecraft: {0}'.format(ref_obj['mc_version']);
-          item['weight'] = 5;
-          item['filename'] = filename;
-          item['downloaded'] = false;
-          item['version'] = ref_obj['mc_version'];
-          item['release_version'] = '';
-          item['url'] =
-
-          p.push(item);
-        });
+            p.push(item);
+          })
         }
       } catch (e) {
         console.log(e);
